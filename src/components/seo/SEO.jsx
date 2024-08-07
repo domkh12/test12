2fef62be87b85ae2889bdc718ec153b6b6664878
EI CHANUDOM <chanudomei122@gmail.com>
2024-08-07 17:44:31 +0700

    m

--- src/components/seo/SEO.jsx
+++ src/components/seo/SEO.jsx
@@ -4,7 +4,6 @@ import { Helmet } from 'react-helmet-async';
 const SEO = ({ title, description, keywords, url, image }) => {
   return (
     <Helmet>
-      <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
       <meta property="og:title" content={title} />
