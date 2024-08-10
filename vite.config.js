d56f341182f9e5ebd60a18f3a8743fdc2c7dcf15
EI CHANUDOM <chanudomei122@gmail.com>
2024-08-10 22:33:40 +0700

    m

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { prerender } from 'vite-plugin-prerender';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  
-})
+  plugins: [
+    react(),
+    prerender({
+      staticDir: 'dist',
+      routes: ['/'], // Add all your routes here
+    }),
+  ],
+});
